713f1385d2cc771a7a0c27b16bbdd54998e52354
robbydio-123 <robbyyaaa@gmail.com>
2025-10-08 18:04:52 +0700

    Josbhet 6

---
+++ ambon.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ambon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double Ratarata;
+        String Nilaihuruf = "";
+        String status = "-";
+        double UTS, UTS2;
+        double nilaiakhir1, nilaiakhir2, nilairatarata;
+        
+        System.out.println("    ===================================================================");
+        System.out.println("    Data Mahasiswa Lulusan Jurursan Teknologi Informasi");
+        System.out.println("    POLITEKNIK NEGERI MALANG PERIODE 2025/2026");
+        System.out.println("    Pembobotan: Nilai Tugas 30%, Nilai UTS 30%, Nilai UAS 40%");
+        System.out.println("    ===================================================================");   
+
+    }
+}
