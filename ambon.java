5e90f84edd824fa32ba272c84e811013574e8b59
robbydio-123 <robbyyaaa@gmail.com>
2025-10-08 18:21:15 +0700

    Josbhet 6

--- ambon.java
+++ ambon.java
@@ -16,5 +16,36 @@ public class ambon {
         System.out.println("    Pembobotan: Nilai Tugas 30%, Nilai UTS 30%, Nilai UAS 40%");
         System.out.println("    ===================================================================");   
 
+       System.out.println("    ======================= INPUT DATA MAHASISWA ======================");
+             String nama;
+             System.out.print("     Masukan Nama: ");
+             nama = scanner.nextLine();
+             String nim;
+             System.out.print("     Masukan NIM: ");
+             nim = scanner.nextLine();
+             System.out.println("     ==================================================================");
+
+                 System.out.println("     ========= Mata Kuliah 1: Algoritma dan Pemrograman ===============");
+                 System.out.print("     Masukan Nilai UTS: ");
+                 UTS = scanner.nextDouble();
+                 double UAS;
+                 System.out.print("     Masukan Nilai UAS: ");
+                 UAS = scanner.nextDouble();
+                 double Tugas;
+                 System.out.print("     Masukan Nilai Tugas: ");
+                 Tugas = scanner.nextDouble();
+                 System.out.println("     ==================================================================");
+
+
+                      System.out.println("     ========= Mata Kuliah 2: Basis Data ==============================");
+                      System.out.print("     Masukan Nilai UTS2: ");
+                      UTS2 = scanner.nextDouble();
+                      double UAS2;
+                      System.out.print("     Masukan Nilai UAS2: ");
+                      UAS2 = scanner.nextDouble();
+                      double Tugas2;
+                      System.out.print("     Masukan Nilai Tugas2: ");
+                      Tugas2 = scanner.nextDouble();
+                      System.out.println("     ==================================================================");
     }
 }
